cc957a77d299c32aa5d417489e8a724fcc9a284b
IainHall <61051711+IainHall@users.noreply.github.com>
2024-11-28 14:43:57 +0000

    elite cs robot work

---
+++ robot_arm_control/cs_series_arm_test.py
@@ -0,0 +1,21 @@
+import robot
+#from robot import Robot
+warehouse_bot = Robot("192.168.0.1")
+
+
+# Connect to the robot
+connection, message = warehouse_bot.connect()
+print(message)
+
+'''
+if connection:
+    # Accessing the value of System Variable B at address 1
+    _, result = warehouse_bot.getVariable("SysVarB", 1)
+    print("Value of SysVarB at addr 1: ", result)
+
+    # Disconnection the robot
+    result, message = warehouse_bot.disconnect()
+    print(message)
+
+print("hello")
+'''
