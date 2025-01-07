c3cfa159f3852fda4017f6b030c76bbc98346693
IainHall <61051711+IainHall@users.noreply.github.com>
2025-01-07 09:57:27 +0000

    fixed attitude data output

--- gt_data_transformation/get_pose_matrix.m
+++ gt_data_transformation/get_pose_matrix.m
@@ -1,12 +1,18 @@
-function [pose,str] = get_pose_matrix(translations,euler_angles,direction)
+function [pose,str] = get_pose_matrix(translations,euler_angles,direction, deg)
 %GET_POSE_MATRIX Summary of this function goes here
 %   Detailed explanation goes here
 
+if deg == true
+    euler_angles = deg2rad(euler_angles);
+end
+
+
 if direction == -1
    
+    
     pose =  eye(4);
     %pose(1:3,1:3) = angle2dcm(euler_angles(1),euler_angles(2),euler_angles(3),'XYZ');
-    pose(1:3,1:3)  = inv(eul2rotm(euler_angles));
+    pose(1:3,1:3)  = inv(eul2rotm(euler_angles,'XYZ'));
     pose(1:3,4) = -translations;
 
     str = "";
@@ -14,7 +20,7 @@ if direction == -1
 elseif direction ==1
     pose =  eye(4);
     %pose(1:3,1:3) = angle2dcm(euler_angles(1),euler_angles(2),euler_angles(3),'XYZ');
-    pose(1:3,1:3)  =eul2rotm(euler_angles);
+    pose(1:3,1:3)  =eul2rotm(euler_angles,'XYZ');
     pose(1:3,4) = translations;
 
     str = "";
@@ -24,7 +30,7 @@ else
     disp(str)
     pose =  eye(4);
     %pose(1:3,1:3) = angle2dcm(euler_angles(1),euler_angles(2),euler_angles(3),'XYZ');
-    pose(1:3,1:3)  =eul2rotm(euler_angles);
+    pose(1:3,1:3)  =eul2rotm(euler_angles,'XYZ');
     pose(1:3,4) = translations;
    
 end
--- gt_data_transformation/lab_run181224.m
+++ gt_data_transformation/lab_run181224.m
@@ -14,20 +14,20 @@ time_data = jsondecode(str);
 
 field_list = {"obj_to_e2","e2_to_arm2","arm2_to_rail","rail_to_tent","tent_to_arm1","arm1_to_e1", "e1_to_cam"};
 entry_name = "effector2_to_object";
-transform.obj_to_e2 = get_pose_matrix(table2array(fixed_data(entry_name,["x","y","z"])),table2array(fixed_data(entry_name,["rotx","roty","rotz"])),-1);
+transform.obj_to_e2 = get_pose_matrix(table2array(fixed_data(entry_name,["x","y","z"])),table2array(fixed_data(entry_name,["rotx","roty","rotz"])),-1,true);
 transform.e2_to_arm2 = [];
 entry_name = "rail_to_arm2";
-transform.arm2_to_rail = get_pose_matrix(table2array(fixed_data(entry_name,["x","y","z"])),table2array(fixed_data(entry_name,["rotx","roty","rotz"])),-1);
+transform.arm2_to_rail = get_pose_matrix(table2array(fixed_data(entry_name,["x","y","z"])),table2array(fixed_data(entry_name,["rotx","roty","rotz"])),-1,true);
 entry_name = "tent_to_rail";
-transform.rail_to_tent = get_pose_matrix(table2array(fixed_data(entry_name,["x","y","z"])),table2array(fixed_data(entry_name,["rotx","roty","rotz"])),-1);
+transform.rail_to_tent = get_pose_matrix(table2array(fixed_data(entry_name,["x","y","z"])),table2array(fixed_data(entry_name,["rotx","roty","rotz"])),-1,true);
 entry_name = "tent_to_arm1";
-transform.tent_to_arm1 = get_pose_matrix(table2array(fixed_data(entry_name,["x","y","z"])),table2array(fixed_data(entry_name,["rotx","roty","rotz"])),1);
+transform.tent_to_arm1 = get_pose_matrix(table2array(fixed_data(entry_name,["x","y","z"])),table2array(fixed_data(entry_name,["rotx","roty","rotz"])),1,true);
 entry_name = "arm1_to_effector1";
-transform.arm1_to_e1 = get_pose_matrix(table2array(fixed_data(entry_name,["x","y","z"])),table2array(fixed_data(entry_name,["rotx","roty","rotz"])),1);
+transform.arm1_to_e1 = get_pose_matrix(table2array(fixed_data(entry_name,["x","y","z"])),table2array(fixed_data(entry_name,["rotx","roty","rotz"])),1,true);
 entry_name = "arm1_to_effector1";
-transform.arm1_to_e1 = get_pose_matrix(table2array(fixed_data(entry_name,["x","y","z"])),table2array(fixed_data(entry_name,["rotx","roty","rotz"])),1);
+transform.arm1_to_e1 = get_pose_matrix(table2array(fixed_data(entry_name,["x","y","z"])),table2array(fixed_data(entry_name,["rotx","roty","rotz"])),1,true);
 entry_name = "effector1_to_camera";
-transform.e1_to_cam = get_pose_matrix(table2array(fixed_data(entry_name,["x","y","z"])),table2array(fixed_data(entry_name,["rotx","roty","rotz"])),1);
+transform.e1_to_cam = get_pose_matrix(table2array(fixed_data(entry_name,["x","y","z"])),table2array(fixed_data(entry_name,["rotx","roty","rotz"])),1,true);
 
 rot_set = {};
 eul_set = {};
@@ -36,7 +36,7 @@ for i_time =1:length(time_data)
     
     translation = time_data(i_time).cpose(1:3)/1000;
     rotation =  time_data(i_time).cpose(4:6)';
-    transform.e2_to_arm2 = get_pose_matrix(translation,rotation,-1);
+    transform.e2_to_arm2 = get_pose_matrix(translation,rotation,-1,true);
 
     rotm = eye(3);
     for i_pose = 1: length(field_list)
